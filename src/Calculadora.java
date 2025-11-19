7848df43c393461f872e6c2ba5f920e366d6721a
Katlhyn <emonval715@g.educaand.es>
2025-11-19 13:38:25 +0100

    Multiplicación y división añadidas

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,10 +6,16 @@ public class Calculadora {
         return a + b;
     }
 
-
-
     public static int restar (int a, int b){
         return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;   // Versión inicial sin control de errores
     }
 
     public static void main(String[] args) {
@@ -26,15 +32,35 @@ public class Calculadora {
         System.out.println("Segundo número");
         int num2 = sc.nextInt();
         sc.nextLine();
-
         if (operador.equals("+")){
             System.out.println("Suma: " + sumar(num1, num2));
         }
-
         if (operador.equals("-")){
             System.out.println("Resta: " + restar(num1, num2));
         }
+        
+        System.out.println("Multiplicación o división ('*' o '/')");
+        String operador2 = sc.nextLine();
 
-    
+        System.out.println("Primer número");
+        int num3 = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Segundo número");
+        int num4 = sc.nextInt();
+        sc.nextLine();
+
+        if (operador2.equals("*")) {
+            System.out.println("Multiplicación: " + multiplicar(num3, num4));
+        }
+
+        if (operador2.equals("/")) {
+            System.out.println("División: " + dividir(num3, num4));
+        }
+
+
     }
+
+
+
 }
